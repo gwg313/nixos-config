d8b923553f420cbf6f02fbf3c70cadaa9dade821
Glen Goodwin <glen.goodwin1992@gmail.com>
2023-06-07 22:57:50 -0400

    chore: remove deadnix, statix pre commit hooks

--- devenv.nix
+++ devenv.nix
@@ -21,8 +21,6 @@
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
   pre-commit.hooks.nixpkgs-fmt.enable = true;
-  pre-commit.hooks.deadnix.enable = true;
-  pre-commit.hooks.statix.enable = true;
   # https://devenv.sh/processes/
   # processes.ping.exec = "ping example.com";
 
