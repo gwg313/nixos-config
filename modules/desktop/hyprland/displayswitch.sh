3e16e02840d6493a0650de272240444206f66c8d
Glen Goodwin <glen.goodwin1992@gmail.com>
2023-09-07 14:28:44 -0400

    chore: change scaling for laptop monitor

--- modules/desktop/hyprland/displayswitch.sh
+++ modules/desktop/hyprland/displayswitch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if grep open /proc/acpi/button/lid/LID/state; then
-    hyprctl keyword monitor "eDP-1, 1920x1080, 0x0, 1"
+    hyprctl keyword monitor "eDP-1, 1920x1080, 0x0, 1.5"
 else
   if [[ $(hyprctl monitors | grep -c "Monitor") != 1 ]]; then
         hyprctl keyword monitor "eDP-1, disable"
