e779c6bb5b4123ce5efa6ca1a98026099fac4e53
Glen Goodwin <glen.goodwin1992@gmail.com>
2023-09-28 00:42:48 -0400

    alter firefox install

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -72,7 +72,6 @@
       usbutils
       wget
       lsof
-      firefox
     ];
   };
 
@@ -115,21 +114,6 @@
   nix.settings.allowed-users = [ "@wheel" ];
   security.sudo.execWheelOnly = true;
 
-  programs.firejail = {
-    enable = true;
-    wrappedBinaries = {
-      firefox = {
-        executable = "${pkgs.lib.getBin pkgs.firefox}/bin/firefox";
-        profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
-        extraArgs = [
-          # Required for U2F USB stick
-          "--ignore=private-dev"
-          # Enable system notifications
-          "--dbus-user.talk=org.freedesktop.Notifications"
-        ];
-      };
-    };
-  };
 
 
   # enable antivirus clamav and
