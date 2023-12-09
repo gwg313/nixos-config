46bd8dd844a5e4cff78fbdd1e090dd35b9724f37
Glen Goodwin <glen.goodwin1992@gmail.com>
2023-12-09 13:45:27 -0500

    format: earlyoom

--- hosts/candlekeep/earlyoom.nix
+++ hosts/candlekeep/earlyoom.nix
@@ -1,11 +1,12 @@
 {
   services.earlyoom = {
-      enable = true;
-      freeSwapThreshold = 2;
-      freeMemThreshold = 2;
-      extraArgs = [
-          "-g" "--avoid '^(.Hyrpland-wrapp|alacritty|zerotier-one|.waybar-wrapped)$'"
-          "--prefer '^(electron|libreoffice|gimp|Isolated Web Co)$'"
-      ];
+    enable = true;
+    freeSwapThreshold = 2;
+    freeMemThreshold = 2;
+    extraArgs = [
+      "-g"
+      "--avoid '^(.Hyrpland-wrapp|alacritty|zerotier-one|.waybar-wrapped)$'"
+      "--prefer '^(electron|libreoffice|gimp|Isolated Web Co)$'"
+    ];
   };
 }
