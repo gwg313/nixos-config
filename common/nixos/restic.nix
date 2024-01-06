2037200238e2035623b58a69637db3ed172c93b6
gwg313 <gwg313@pm.me>
2024-01-05 21:57:09 -0500

    chore: refactor restic

--- common/nixos/restic.nix
+++ common/nixos/restic.nix
@@ -1,11 +1,15 @@
-{...}: {
+{
+  config,
+  user,
+  ...
+}: {
   services.restic.backups = {
     backups = {
-      user = "gwg313";
+      user = "${user}";
       repository = "/backups";
       initialize = true;
-      passwordFile = "/home/gwg313/restic.txt";
-      paths = ["/home/gwg313/Projects" "/home/gwg313/Documents" "/home/gwg313/.local/share/password-store" "/home/gwg313/.local/share/buku"];
+      passwordFile = "${config.sops.secrets.restic_key.path}";
+      paths = ["/home/${user}/Projects" "/home/${user}/Documents" "/home/${user}/.local/share/password-store" "/home/${user}/.local/share/buku"];
       timerConfig = {
         onCalendar = "saturday 23:00";
       };
