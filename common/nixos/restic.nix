99bfe8b761cd744013af0cf7eb29713c11d2b144
gwg313 <gwg313@pm.me>
2024-02-18 16:45:11 -0500

    fix: change restic directories

--- common/nixos/restic.nix
+++ common/nixos/restic.nix
@@ -13,7 +13,7 @@
       repository = "/backups";
       initialize = true;
       passwordFile = "${config.sops.secrets.restic_key.path}";
-      paths = ["/home/${user}/Projects" "/home/${user}/Documents" "/home/${user}/.local/share/password-store" "/home/${user}/.local/share/buku"];
+      paths = ["/home/${user}/repos" "/home/${user}/Documents" "/home/${user}/.local/share/password-store" "/home/${user}/.local/share/buku"];
       timerConfig = {
         onCalendar = "saturday 23:00";
       };
