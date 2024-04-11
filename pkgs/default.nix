4a8a2091861153ac66418ad588586cc40f029257
gwg313 <gwg313@pm.me>
2024-04-11 00:34:07 -0400

    fix: disable local sesh

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,5 @@
 # You can build them using 'nix build .#example'
 {pkgs, ...}: {
   # example = pkgs.callPackage ./example { };
-  sesh = pkgs.callPackage ./sesh {};
+  # sesh = pkgs.callPackage ./sesh {};
 }
