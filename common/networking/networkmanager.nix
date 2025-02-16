59b0c85d63a2a73e394435b12380fed73554814a
gwg313 <gwg313@pm.me>
2025-02-15 22:30:52 -0500

    some networkmanager updates

--- common/networking/networkmanager.nix
+++ common/networking/networkmanager.nix
@@ -9,6 +9,7 @@
   networking = {
     networkmanager = {
       enable = true;
+      wifi.scanRandMacAddress = true;
       ensureProfiles = {
         environmentFiles = [ config.sops.secrets."wireless.env".path ];
         profiles = {
@@ -21,6 +22,29 @@
               key-mgmt = "wpa-psk";
               psk = "$home_psk";
             };
+            connection.autoconnect-priority = 100;
+          };
+          eduroam = {
+            connection.id = "eduroam";
+            connection.type = "wifi";
+            wifi.ssid = "eduroam";
+            wifi-security = {
+              auth-alg = "open";
+              key-mgmt = "wpa-eap";
+              eap = "peap";
+              identity = "$eduroam_identity";
+              password = "$school_password";
+              phase2 = "mschapv2";
+            };
+            connection.autoconnect = true;
+            connection.autoconnect-priority = 90;
+            wifi.powersave = 2;
+            ipv4.dns = "8.8.8.8,8.8.4.4.";
+            ipv6.dns = "2001:4860:4860::8888";
+            wifi.mac-address-randomization = "random";
+            ipv4.dhcp-send-hostname = false;
+            ipv4.dhcp-hostname = "NoName";
+            # connection.metered = "yes";
           };
         };
       };
