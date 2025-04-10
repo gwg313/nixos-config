97d15e9be3dd36b5b96c861bd49b88e0af4c37cc
gwg313 <gwg313@pm.me>
2025-04-10 01:37:39 -0400

    update tuigreet

--- common/nixos/tuigreet.nix
+++ common/nixos/tuigreet.nix
@@ -5,7 +5,6 @@
     settings = {
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --asterisks --container-padding 2 --time --time-format '%I:%M %p | %a • %h | %F' --cmd Hyprland";
-        # command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
         user = "greeter";
       };
     };
@@ -17,14 +16,12 @@
     Type = "idle";
     StandardInput = "tty";
     StandardOutput = "tty";
-    StandardError = "journal"; # Without this errors will spam on screen
-    # Without these bootlogs will spam on screen
+    StandardError = "journal";
     TTYReset = true;
     TTYVHangup = true;
     TTYVTDisallocate = true;
   };
-  security.pam.services.greetd.enableGnomeKeyring = true;
-  # To prevent getting stuck at shutdown
+
   systemd.extraConfig = "DefaultTimeoutStopSec=10s";
 
 }
