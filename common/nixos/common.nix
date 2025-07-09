cf6d943f02e5fe9a5b4da539f30555ecb35fbde9
gwg313 <gwg313@pm.me>
2025-07-09 08:09:22 -0400

    feat: add personal nix cache to substitutors
    Signed-off-by: gwg313 <gwg313@pm.me>

--- common/nixos/common.nix
+++ common/nixos/common.nix
@@ -33,6 +33,7 @@
   nix = {
     settings = {
       substituters = [
+        "https://cache.gwg313.xyz/mycache?priority=5"
         "https://cache.nixos.org?priority=10"
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
@@ -40,6 +41,7 @@
         "https://numtide.cachix.org"
       ];
       trusted-public-keys = [
+        "mycache:ovUjiiyQNUGjK8yZA4dPV1f8tIfCZrGIubdWvWpdvtc="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
@@ -58,7 +60,6 @@
       dates = "daily";
       options = "--delete-older-than 14d";
     };
-
   };
 
   services = {
